f3ea095be0040603dc4e53fd339079c58fe25102
sholabioku <bilushonline@gmail.com>
2021-11-09 14:42:43 +0100

    Add profileSchema and its functionality,and export the Profile

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose')
+
+const profileSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  availabiltity: {
+    type: Boolean,
+    default: false,
+  }
+}, { timestamps: true })
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
+
