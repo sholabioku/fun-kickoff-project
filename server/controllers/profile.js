8532b4a3edae19c1f4e2910d05d4735d026ae98e
sholabioku <bilushonline@gmail.com>
2021-11-10 15:02:38 +0100

    Install prettier package and format codes

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -7,21 +7,23 @@ const Profile = require('../models/Profile');
 exports.getProfiles = asyncHandler(async (req, res, next) => {
   const profiles = await Profile.find();
 
-  res.status(200).json({success: true, data: profiles, count: profiles.length});
-})
+  res
+    .status(200)
+    .json({ success: true, data: profiles, count: profiles.length });
+});
 
 // @desc Get single profile
 // @route GET /profiles/:id
 // @access Public
 exports.getProfile = asyncHandler(async (req, res, next) => {
   const profile = await Profile.findById(req.params.id);
-  if(!profile) {
-      res.status(404);
-      throw new Error(`No profile with id ${req.params.id}`);
-  };
+  if (!profile) {
+    res.status(404);
+    throw new Error(`No profile with id ${req.params.id}`);
+  }
 
-  res.status(200).json({success: true, data: profile})
-})
+  res.status(200).json({ success: true, data: profile });
+});
 
 //@desc Create a profile
 //@route POST /profile
@@ -29,8 +31,8 @@ exports.getProfile = asyncHandler(async (req, res, next) => {
 exports.createProfile = asyncHandler(async (req, res, next) => {
   const profile = await Profile.create(req.body);
 
-  res.status(201).json({success: true, data: profile})
-})
+  res.status(201).json({ success: true, data: profile });
+});
 
 // @desc Update profile
 // @route PUT /profiles/:id
@@ -39,13 +41,12 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
   const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
-  })
+  });
 
-  if(!profile) {
+  if (!profile) {
     res.status(404);
     throw new Error(`No profile with id ${req.params.id}`);
-  };
+  }
 
-  res.status(200).json({success: true, data: profile})
-
-})
+  res.status(200).json({ success: true, data: profile });
+});
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProfiles, getProfile, createProfile, updateProfile } = require('../controllers/profile')
-const { validateCreateProfile } = require('../validate')
-const protect = require('../middleware/auth')
+const {
+  getProfiles,
+  getProfile,
+  createProfile,
+  updateProfile,
+} = require('../controllers/profile');
+const { validateCreateProfile } = require('../validate');
+const protect = require('../middleware/auth');
 
-router.route('/')
-        .get(getProfiles)
-        .post([protect, validateCreateProfile], createProfile);
+router
+  .route('/')
+  .get(getProfiles)
+  .post([protect, validateCreateProfile], createProfile);
 
-router.route('/:id').get(getProfile).put([protect, validateCreateProfile], updateProfile);
+router
+  .route('/:id')
+  .get(getProfile)
+  .put([protect, validateCreateProfile], updateProfile);
 
-
-module.exports = router
+module.exports = router;
