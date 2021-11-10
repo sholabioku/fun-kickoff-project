999ed36de292c1fe7b3d64bcbc5c67244aab32e8
sholabioku <bilushonline@gmail.com>
2021-11-10 10:41:20 +0100

    Add POST /profiles route by chaining

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProfiles} = require('../controllers/profile')
+const {getProfiles, createProfile} = require('../controllers/profile')
 
-router.route('/').get(getProfiles)
+router.route('/').get(getProfiles).post(createProfile)
 
 
 module.exports = router
