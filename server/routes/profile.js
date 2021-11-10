4f7df1160d24ce3232b32e1bd4d0b9da754fa5c1
sholabioku <bilushonline@gmail.com>
2021-11-10 11:08:41 +0100

    Add validateCreateProfile to POST /profiles route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProfiles, createProfile} = require('../controllers/profile')
+const { getProfiles, createProfile } = require('../controllers/profile')
+const { validateCreateProfile } = require('../validate')
 const protect = require('../middleware/auth')
 
-router.route('/').get(getProfiles).post(protect, createProfile)
+router.route('/').get(getProfiles).post([protect, validateCreateProfile], createProfile)
 
 
 module.exports = router
