251ac54a3a2d1707ae388d456d7e82a58d63fed7
sholabioku <bilushonline@gmail.com>
2021-11-10 10:43:59 +0100

    Protect POST /profiles route with authentication middleware

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {getProfiles, createProfile} = require('../controllers/profile')
+const protect = require('../middleware/auth')
 
-router.route('/').get(getProfiles).post(createProfile)
+router.route('/').get(getProfiles).post(protect, createProfile)
 
 
 module.exports = router
