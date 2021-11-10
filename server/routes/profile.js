d14457fa7dace7c2cfcf082f22001d48aedeca8f
sholabioku <bilushonline@gmail.com>
2021-11-10 14:10:10 +0100

    Add GET /profiles/:id route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProfiles, createProfile } = require('../controllers/profile')
+const { getProfiles, getProfile, createProfile } = require('../controllers/profile')
 const { validateCreateProfile } = require('../validate')
 const protect = require('../middleware/auth')
 
-router.route('/').get(getProfiles).post([protect, validateCreateProfile], createProfile)
+router.route('/')
+        .get(getProfiles)
+        .post([protect, validateCreateProfile], createProfile);
+
+router.route('/:id').get(getProfile)
 
 
 module.exports = router
