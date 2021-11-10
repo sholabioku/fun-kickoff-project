d7e19c4553b1f2fe9e65c9a96865d267b4119b24
sholabioku <bilushonline@gmail.com>
2021-11-10 10:39:19 +0100

    Add createProfile method and its functionality to create new profile

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -7,5 +7,14 @@ const Profile = require('../models/Profile');
 exports.getProfiles = asyncHandler(async (req, res, next) => {
   const profiles = await Profile.find();
 
-  res.status(200).json({success: true, data: profiles})
+  res.status(200).json({success: true, data: profiles, count: profiles.length});
+})
+
+//@desc Create a profile
+//@route POST /profile
+//@ access Private
+exports.createProfile = asyncHandler(async (req, res, next) => {
+  const profile = await Profile.create(req.body);
+
+  res.status(201).json({success: true, data: profile})
 })
