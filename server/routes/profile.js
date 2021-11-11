86a923385011770ffa0bccec6dc50c17fb097a28
sholabioku <bilushonline@gmail.com>
2021-11-11 14:49:10 +0100

    Import validateObjectId from middleware/validateObjectId.js file

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const validateObjectId = require('../middleware/validateObjectId');
+
 const {
   getProfiles,
   getProfile,
