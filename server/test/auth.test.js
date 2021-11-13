82739bf0e9e041b1e72bff2350363ed4714fe184
sholabioku <bilushonline@gmail.com>
2021-11-13 11:27:46 +0100

    Add test for POST /auth/register to create a new user

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -2,10 +2,15 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { app } = require('../app.js');
 
+const User = require('../models/User');
+
 chai.should();
 chai.use(chaiHttp);
 
 describe('Mocha test for auth controller', () => {
+  afterEach(async () => {
+    await User.deleteMany({});
+  });
   const message = 'You have successfully logged out';
 
   describe('GET /auth/logout ', () => {
@@ -14,6 +19,22 @@ describe('Mocha test for auth controller', () => {
 
       res.should.have.status(200);
       res.text.should.equal(message);
+    });
+  });
+
+  describe('POST /auth/register', () => {
+    it('should create a new user', async () => {
+      const username = 'newUser';
+      const email = 'example@example.com';
+      const password = '1234567';
+      const res = await chai
+        .request(app)
+        .post('/auth/register')
+        .send({ username, email, password });
+      res.should.have.status(201);
+      res.body.success.user.should.have.property('id');
+      res.body.success.user.should.have.property('username');
+      res.body.success.user.should.have.property('email');
     });
   });
 });
