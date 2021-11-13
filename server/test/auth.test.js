6d722ae73b4d6c22707c3b2fa8e6dea863997b32
sholabioku <bilushonline@gmail.com>
2021-11-13 11:56:19 +0100

    Add test for POST /auth/register to return 400 if email is invalid

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -67,5 +67,16 @@ describe('Mocha test for auth controller', () => {
 
       res.should.have.status(400);
     });
+    it('should return 400 if email is invalid', async () => {
+      const username = 'abcsdetg';
+      const email = 'bilushgmail.com';
+      const password = '643434u34';
+      const res = await chai
+        .request(app)
+        .post('/auth/register')
+        .send({ username, email, password });
+
+      res.should.have.status(400);
+    });
   });
 });
