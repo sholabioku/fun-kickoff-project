19a59dbeeab8211a764020ac28c70069020759fa
sholabioku <bilushonline@gmail.com>
2021-11-14 07:50:44 +0100

    Add configuration for database

--- server/db.js
+++ server/db.js
@@ -1,11 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let database = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  database = process.env.MONGO_URI_TEST;
+} else {
+  database = process.env.MONGO_URI;
+}
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(database, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   });
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
